5f45a07f38ed7701994d455f44131c13a59c3083
lipengcheng577 <lipengcheng577@users.noreply.github.com>
2018-12-21 16:55:51 +0800

    data_point_dict表初始化
    data_point_dict表初始化
    建立ini文件夹

--- IaaS_data/init_table.py
+++ IaaS_data/init_table.py
@@ -6,37 +6,72 @@ import ConfigParser
 from db_opt import *
 
 
-def read_ini():
-    print 'init table program start ...'
+class init_table(object):
+    def __init__(self):
+        self._db = db_opt()
 
-    db = db_opt()
+    '''read table_info.ini'''
+    def init_table(self):
+        print 'init table program start ...'
 
-    config = ConfigParser.ConfigParser()
+        config = ConfigParser.ConfigParser()
 
-    try:
-        config.read("table_info.ini")
-    except IOError:
-        print "打开table_info.ini 失败！"
-        return
+        try:
+            config.read("./ini/table_info.ini")
+        except IOError:
+            print "打开table_info.ini 失败！"
+            return
 
-    tables = config.sections()
+        tables = config.sections()
 
-    for table in tables:
-#        print table + ":"
-        columns = config.items(table)
-#        print columns
+        for table in tables:
+            columns = config.items(table)
 
-        counter = 1
-        sql = "create table " + table + " ( "
-        for column in columns:
-            sql += column[0] + ' ' + column[1]
-            if counter != len(columns):
-                sql += ', '
-            counter += 1
-        sql += " )"
+            counter = 1
+            sql = "create table " + table + " ( "
+            for column in columns:
+                sql += column[0] + ' ' + column[1]
+                if counter != len(columns):
+                    sql += ', '
+                counter += 1
+            sql += " )"
 
-        db.excute(sql)
+            self._db.excute(sql)
 
+    def init_data_point_dict(self):
+        print 'init_data_point_dict start ...'
+        config = ConfigParser.ConfigParser()
+
+        try:
+            config.read("./ini/data_point_dict.ini")
+        except IOError:
+            print "打开data_point_dict.ini 失败！"
+            return
+
+        rows = config.sections()
+
+        for row in rows:
+            columns = config.items(row)
+            #INSERT INTO table_name (列1, 列2,...) VALUES (值1, 值2,....)
+            counter = 1
+            sql_key = "( "
+            sql_value = "( "
+            for column in columns:
+                sql_key += column[0]
+                sql_value += column[1]
+                if counter != len(columns):
+                    sql_key += ', '
+                    sql_value += ', '
+                counter += 1
+            sql_key += " )"
+            sql_value += " )"
+            print sql_key
+            print sql_value
+            sql = "insert into data_point_dict " + sql_key + " values " + sql_value
+            self._db.excute(sql)
 
 if __name__ == "__main__":
-    read_ini()
+
+    table_opt = init_table()
+    #table_opt.init_table()
+    table_opt.init_data_point_dict();
